9e5e377b73a704c9e994c7568bcb09891135c685
Kirill Gaposhkin <130587425+Kirill5github@users.noreply.github.com>
2023-07-01 18:26:50 +0300

    Update 22.9.1.py

--- 22.9.1.py
+++ 22.9.1.py
@@ -1,5 +1,4 @@
-array = [int(x) for x in input("Введите целые числа в любом порядке, через пробел: ").split()]
-
+array = [int(x) for x in input("Введите целые числа через пробел: ").split()]
 def merge_sort(array):
     if len(array) < 2:
         return array[:]
@@ -8,11 +7,9 @@ def merge_sort(array):
         left = merge_sort(array[:middle])
         right = merge_sort(array[middle:])
         return merge(left, right)
-
 def merge(left, right):
     result = []
     i, j = 0, 0
-
     while i < len(left) and j < len(right):
         if left[i] < right[j]:
             result.append(left[i])
@@ -20,33 +17,27 @@ def merge(left, right):
         else:
             result.append(right[j])
             j += 1
-
     while i < len(left):
         result.append(left[i])
         i += 1
-
     while j < len(right):
         result.append(right[j])
         j += 1
-
     return result
-
 array = merge_sort(array)
 print(array)
-
 while True:
     try:
         element = int(input("Введите любое положительное целое число из полученного списка: "))
         if element < min(array) or element > max(array):
-            print("Указанное число не входит в диапазон списка!")
+            print("Внимание! Указанное число не входит в диапазон списка!")
         if element <= 0:
             raise Exception
         break
     except ValueError:
-        print("Нужно ввести целое число!")
+        print("Ошибка!Нужно ввести целое число!")
     except Exception:
-        print("Нужно ввести положительное число!")
-
+        print("Ошибка!Нужно ввести положительное число!")
 def binary_search(array, element, left, right):
     if left > right:
         return False
@@ -57,5 +48,4 @@ def binary_search(array, element, left, right):
         return binary_search(array, element, left, middle - 1)
     else:
         return binary_search(array, element, middle + 1, right)
-
 print(binary_search(array, element, 0, len(array) - 1))
